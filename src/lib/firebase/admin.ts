aef78db93b7e1f9d42fbc3f92110e21d5f3b02f6
k3nt0w <k3nt0.watanabe@gmail.com>
2019-07-21 18:46:35 +0900

    fix: initialize app

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,8 +1,17 @@
 import admin from 'firebase-admin'
 
-const firebaseAdmin = admin.initializeApp({
-  credential: admin.credential.cert(require('../../credentials/server'))
-})
+console.log(admin.apps.length)
+
+const firebaseAdmin = !admin.apps.length
+  ? admin.initializeApp(
+      {
+        credential: admin.credential.cert(
+          require('../../../credentials/server.json')
+        )
+      },
+      'server'
+    )
+  : admin.app('server')
 
 const auth = firebaseAdmin.auth()
 
