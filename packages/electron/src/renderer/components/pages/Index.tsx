62d227f0f0d79da11191f5da86ba7b526c9f0820
Kento Watanabe <kento.watanabe.11@gmail.com>
2021-08-11 17:25:30 +0900

    fix: ci (#121)
    * fix: build config
    
    * fix: component name

--- packages/electron/src/renderer/components/pages/Index.tsx
+++ packages/electron/src/renderer/components/pages/Index.tsx
@@ -2,12 +2,12 @@ import { routerOperations } from '@renderer/states/modules/router'
 import React from 'react'
 import { useDispatch } from 'react-redux'
 
-export const Login = () => {
+export const Index = () => {
   const dispatch = useDispatch()
   const handleClick = () => dispatch(routerOperations.pushHome())
   return (
     <>
-      <h1>LOGIN PAGE</h1>
+      <h1>INDEX PAGE</h1>
       <button onClick={handleClick}>TO HOME</button>
     </>
   )
